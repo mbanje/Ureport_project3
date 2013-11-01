04a6d1d35413c7a1e6291bd1d6059171931329f5
mbanje <mbanjejvernack@gmail.com>
2013-11-01 11:39:04 +0200

    Delete unnecessary urls and views

--- ureport_project/rapidsms_ureport/ureport/views/response_views.py
+++ ureport_project/rapidsms_ureport/ureport/views/response_views.py
@@ -27,48 +27,7 @@ from django.contrib.auth.models import Group, User #, Message
 from ureport.models import UPoll
 import logging, datetime
 
-
-def view_scouts_responses(req, pol):
-
-
-    responses= Response.objects.filter(contact__groups__name='scout',poll__pk=pol)
-    number_of_members= Contact.objects.count()
-    p= Poll.objects.get(pk=pol)
-    template = 'ureport/scout_poll_results.html'
-    return render_to_response(template, {
-        'responses': responses,
-        'total_ureporters':number_of_members,
-        'poll':p}
-        )
-
-   
-def view_guides_responses(req, pol):
-
-    responses= Response.objects.filter(contact__groups__name='guide',poll__pk=pol)
-    number_of_members= Contact.objects.count()
-    p= Poll.objects.get(pk=pol)
-    template = 'ureport/guide_poll_results.html'
-    return render_to_response(template, {
-        'responses': responses,
-        'total_ureporters':number_of_members,
-        'poll':p}
-        )
-        
-def view_redcross_responses(req, pol):
-
-    responses= Response.objects.filter(contact__groups__name='redcross',poll__pk=pol)
-    number_of_members= Contact.objects.count()
-    p= Poll.objects.get(pk=pol)
-    template = 'ureport/redcross_poll_results.html'
-    return render_to_response(template, {
-        'responses': responses,
-        'total_ureporters':number_of_members,
-        'poll':p}
-        )
-        
-       
-       
-        
+ 
 
 def view_responses_of_gp_on_poll(req, gp, pol):
 
